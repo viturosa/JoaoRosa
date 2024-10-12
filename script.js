4a0e159f4e8ab07ff33a451134bbe8af7b9c3722
Vitor Rosa <joaovhp@hotmail.com>
2024-10-12 12:46:30 -0300

    desafioComplementar

---
+++ script.js
@@ -0,0 +1,10 @@
+var entrada = prompt ("Insira seu nome");
+var saida = "Bem vindo(a) " + entrada;
+
+alert(saida)
+
+var entrada2 = prompt ("Quantos anos você tem " + entrada + "?");
+var idade = parseInt (entrada2);
+var nascimento = 2024 - entrada2;
+console.log(entrada + " nasceu em " + nascimento);
+
